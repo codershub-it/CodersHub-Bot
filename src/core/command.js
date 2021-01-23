9eb52f81cdbf4144f77f0fe7f6d41aeb3ddd287f
David <davidgalet@gmail.com>
2021-01-23 14:03:12 +0100

    Init Beta
    Questa è la repo BETA qui si fanno i test o modifiche sperimentali

---
+++ src/core/command.js
@@ -0,0 +1,8 @@
+/**
+ * Questa è la classe madre di commands se si vuole creare dei metodi o variabili per tutti i comandi
+ * @type {Commands}
+ */
+module.exports = class Commands {
+    constructor(client) {
+    }
+}
---
+++ src/core/model/users.js
@@ -0,0 +1,10 @@
+class Users {
+    constructor(db) {
+        this.collection = db.collection('users');
+    }
+    async addUser(user) {
+        const newUser = await this.collection.insertOne(user);
+        return newUser;
+    }
+}
+module.exports = Users;
