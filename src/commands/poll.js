eeb390a6294fb4b5a9fdc7c26eb34534af8bb570
David <davidgalet@gmail.com>
2021-01-24 18:07:23 +0100

    Aggiunta del sistema dei sondaggi

--- src/commands/poll.js
+++ src/commands/poll.js
@@ -4,14 +4,15 @@ const numberToEmoji = require('../core/utility/numberToEmoji')
 module.exports = class Poll extends Commands {
   constructor(client) {
     super(client)
-    this.cmd = 'poll'
-    this.alias = 'po'
-    this.args = '[domanda tra doppie virgolette] ...[risposte tra doppie virgolette]'
-    this.example = `${client.conf.prefix}pull "che tempo fa oggi?" "bello" "brutto"`
+    this.cmd = 'sondaggio'
+    this.alias = 'poll'
+    this.args =
+      '[domanda tra doppie virgolette] ...[risposte tra doppie virgolette (opzionali) massimo 10]'
+    this.example = `${client.conf.prefix}sondaggio "che tempo fa oggi?" "bello" "brutto"`
     this.description = 'Questo comando serve a creare dei sondaggi'
     this.timer = 0
-    this.access = [client._botSettings.rules.Admin, client._botSettings.rules.Moderatore]
-    this.displayHelp = 0
+    this.access = [client._botSettings.rules.everyone]
+    this.displayHelp = 1
   }
 
   /**
@@ -21,40 +22,46 @@ module.exports = class Poll extends Commands {
    * @returns {Promise<void>}
    */
   async execution(message, bot) {
-    // Estrapolo i componenti dal testo
     const arg = message.args.match(/"(.*?)"/g)
-
-    // Verifico che ci sia la domanda o con le risposte
-    if (!arg) message.reply(' devi scrivere almeno una domanda e delle risposte')
-
+    if (!arg) {
+      message.reply(' devi scrivere almeno una domanda e delle risposte tra doppie virgolette')
+      return
+    }
     const question_string = arg[0].replace(/"/g, '')
+    if (question_string.length < 20) {
+      message.reply(' devi scrivere almeno una domanda da 20 caratteri')
+      return
+    }
+    // Elimino il primo array che è la domanda
     arg.shift()
-
     if (question_string) {
       if (arg && arg.length > 0 && arg.length < 11) {
-        const embed = new bot._botMessageEmbed()
-        let msg = ''
-
+        const emb = new bot._botMessageEmbed()
+        let msg = `**Domanda proposta da <@${message.author.id}>**\n${question_string}\n`
+        msg += '**-- Risposte --**\n'
         Array.from(arg).forEach((answer, i) => {
           msg += `${numberToEmoji.toEmoji(i)} ${answer.replace(/"/g, '')}\n`
         })
-
-        embed.setColor('RANDOM')
-        embed.setTitle(question_string)
-        embed.setDescription(msg)
-        embed.setThumbnail(
-          'https://media1.tenor.com/images/0edd53dd2110147b786329c2e24fb1d0/tenor.gif',
-        )
-
-        const queueEmbed = await message.channel.send({ embed })
-
-        for (const answer of Array.from(arg)) {
-          const i = Array.from(arg).indexOf(answer)
+        emb.setColor('RANDOM')
+        emb.setDescription(msg)
+        const queueEmbed = await message.channel.send(emb)
+        for (let i = 0; i < arg.length; i++) {
           await queueEmbed.react(numberToEmoji.toEmoji(i))
         }
+        await message.delete()
+      } else {
+        const emb = new bot._botMessageEmbed()
+        const msg = `-- **Domanda proposta da <@${message.author.id}>** --\n${question_string}\n`
+        emb.setDescription(msg)
+        emb.setColor('RANDOM')
+        emb.setTimestamp()
+        const messageToSend = await message.channel.send(emb).catch((e) => {
+          console.log(e)
+        })
+        await messageToSend.react('👍')
+        await messageToSend.react('👎')
+        await message.delete()
       }
-
-      console.log(arg)
     }
   }
 }
