d310b63127e34f60ca95e8a5558eb948a6ac5798
David <davidgalet@gmail.com>
2021-01-24 17:13:27 +0100

    upd poll comando

--- src/commands/poll.js
+++ src/commands/poll.js
@@ -1,68 +1,60 @@
-const Commands = require('../core/command');
-const numberToEmoji  = require('../core/utility/numberToEmoji')
+const Commands = require('../core/command')
+const numberToEmoji = require('../core/utility/numberToEmoji')
 
 module.exports = class Poll extends Commands {
-    constructor(client) {
-        super(client);
-        this.cmd = 'poll';
-        this.alias = 'po';
-        this.args = '[domanda tra doppie virgolette] ...[risposte tra doppie virgolette]';
-        this.example = `${client.conf.prefix}pull "che tempo fa oggi?" "bello" "brutto"`;
-        this.description = 'Questo comando serve a creare dei sondaggi';
-        this.timer = 0;
-        this.access = [
-            client._botSettings.rules.Admin,
-            client._botSettings.rules.Moderatore,
-        ];
-        this.displayHelp = 0;
-    }
+  constructor(client) {
+    super(client)
+    this.cmd = 'poll'
+    this.alias = 'po'
+    this.args = '[domanda tra doppie virgolette] ...[risposte tra doppie virgolette]'
+    this.example = `${client.conf.prefix}pull "che tempo fa oggi?" "bello" "brutto"`
+    this.description = 'Questo comando serve a creare dei sondaggi'
+    this.timer = 0
+    this.access = [client._botSettings.rules.Admin, client._botSettings.rules.Moderatore]
+    this.displayHelp = 0
+  }
 
-    /**
-     * Qui si esegue il processo del comando
-     * @param message
-     * @param bot
-     * @returns {Promise<void>}
-     */
-    async execution(message, bot) {
+  /**
+   * Qui si esegue il processo del comando
+   * @param message
+   * @param bot
+   * @returns {Promise<void>}
+   */
+  async execution(message, bot) {
+    // Estrapolo i componenti dal testo
+    const arg = message.args.match(/"(.*?)"/g)
 
-        // Estrapolo i componenti dal testo
-        let arg = message.args.match(/"(.*?)"/g)
+    // Verifico che ci sia la domanda o con le risposte
+    if (!arg) message.reply(' devi scrivere almeno una domanda e delle risposte')
 
-        // Verifico che ci sia la domanda o con le risposte
-        if (!arg) message.reply(" devi scrivere almeno una domanda e delle risposte")
-        
-        let question_string = arg[0].replace(/"/g, "");
-        arg.shift()
+    const question_string = arg[0].replace(/"/g, '')
+    arg.shift()
 
+    if (question_string) {
+      if (arg && arg.length > 0 && arg.length < 11) {
+        const embed = new bot._botMessageEmbed()
+        let msg = ''
 
-        if (question_string){
+        Array.from(arg).forEach((answer, i) => {
+          msg += `${numberToEmoji.toEmoji(i)} ${answer.replace(/"/g, '')}\n`
+        })
 
-            if (arg && arg.length > 0 && arg.length < 11) {
-                const embed = new bot._botMessageEmbed();
-                let msg = ''
+        embed.setColor('RANDOM')
+        embed.setTitle(question_string)
+        embed.setDescription(msg)
+        embed.setThumbnail(
+          'https://media1.tenor.com/images/0edd53dd2110147b786329c2e24fb1d0/tenor.gif',
+        )
 
-                Array.from(arg).forEach((answer, i) => {
-                    msg += `${numberToEmoji.toEmoji(i)} ${answer.replace(/"/g, "")}\n`
-                });
+        const queueEmbed = await message.channel.send({ embed })
 
-
-                embed.setColor('RANDOM');
-                embed.setTitle(question_string)
-                embed.setDescription(msg)
-                embed.setThumbnail(
-                    'https://media1.tenor.com/images/0edd53dd2110147b786329c2e24fb1d0/tenor.gif',
-                );
-
-                const queueEmbed = await message.channel.send( { embed });
-
-                for (const answer of Array.from(arg)) {
-                    let i = Array.from(arg).indexOf(answer);
-                    await queueEmbed.react(numberToEmoji.toEmoji(i));
-                }
-            }
-
-
-            console.log(arg)
+        for (const answer of Array.from(arg)) {
+          const i = Array.from(arg).indexOf(answer)
+          await queueEmbed.react(numberToEmoji.toEmoji(i))
         }
+      }
+
+      console.log(arg)
     }
-};
+  }
+}
--- src/core/utility/numberToEmoji.js
+++ src/core/utility/numberToEmoji.js
@@ -2,64 +2,64 @@
  * Trasforma il numero in emoji
  */
 class NumberToEmoji {
-    /**
-     * Converts numbers from a string into emojis.
-     * @param {string|number} str - The string with numbers to be converted.
-     * @returns {string}
-     */
-    static toEmoji(str) {
-        if (str === undefined || str === null || str === '') {
-            return str;
-        }
-
-        if (typeof str !== 'string') {
-            str = str.toString();
-        }
-
-        if (str === '10') {
-            return '🔟';
-        }
-
-        return str
-            .replace(/0/g, '0️⃣')
-            .replace(/1/g, '1️⃣')
-            .replace(/2/g, '2️⃣')
-            .replace(/3/g, '3️⃣')
-            .replace(/4/g, '4️⃣')
-            .replace(/5/g, '5️⃣')
-            .replace(/6/g, '6️⃣')
-            .replace(/7/g, '7️⃣')
-            .replace(/8/g, '8️⃣')
-            .replace(/9/g, '9️⃣');
+  /**
+   * Converts numbers from a string into emojis.
+   * @param {string|number} str - The string with numbers to be converted.
+   * @returns {string}
+   */
+  static toEmoji(str) {
+    if (str === undefined || str === null || str === '') {
+      return str
     }
 
-    /**
-     * Converts emojis from a string into numbers.
-     * @param {string|number} str - The string with emojis to be converted to numbers.
-     * @returns {string}
-     */
-    static fromEmoji(str) {
-        if (str === undefined || str === null || str === '') {
-            return str;
-        }
-
-        if (typeof str !== 'string') {
-            str = str.toString();
-        }
-
-        return str
-            .replace(/🔟/g, '10')
-            .replace(/0️⃣/g, '0')
-            .replace(/1️⃣/g, '1')
-            .replace(/2️⃣/g, '2')
-            .replace(/3️⃣/g, '3')
-            .replace(/4️⃣/g, '4')
-            .replace(/5️⃣/g, '5')
-            .replace(/6️⃣/g, '6')
-            .replace(/7️⃣/g, '7')
-            .replace(/8️⃣/g, '8')
-            .replace(/9️⃣/g, '9');
+    if (typeof str !== 'string') {
+      str = str.toString()
     }
+
+    if (str === '10') {
+      return '🔟'
+    }
+
+    return str
+      .replace(/0/g, '0️⃣')
+      .replace(/1/g, '1️⃣')
+      .replace(/2/g, '2️⃣')
+      .replace(/3/g, '3️⃣')
+      .replace(/4/g, '4️⃣')
+      .replace(/5/g, '5️⃣')
+      .replace(/6/g, '6️⃣')
+      .replace(/7/g, '7️⃣')
+      .replace(/8/g, '8️⃣')
+      .replace(/9/g, '9️⃣')
+  }
+
+  /**
+   * Converts emojis from a string into numbers.
+   * @param {string|number} str - The string with emojis to be converted to numbers.
+   * @returns {string}
+   */
+  static fromEmoji(str) {
+    if (str === undefined || str === null || str === '') {
+      return str
+    }
+
+    if (typeof str !== 'string') {
+      str = str.toString()
+    }
+
+    return str
+      .replace(/🔟/g, '10')
+      .replace(/0️⃣/g, '0')
+      .replace(/1️⃣/g, '1')
+      .replace(/2️⃣/g, '2')
+      .replace(/3️⃣/g, '3')
+      .replace(/4️⃣/g, '4')
+      .replace(/5️⃣/g, '5')
+      .replace(/6️⃣/g, '6')
+      .replace(/7️⃣/g, '7')
+      .replace(/8️⃣/g, '8')
+      .replace(/9️⃣/g, '9')
+  }
 }
 
-module.exports = NumberToEmoji;
+module.exports = NumberToEmoji
