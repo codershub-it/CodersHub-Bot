f74315ea8b73727398a858b7679cf656fa4ccfc6
Gianluca Cazzaniga <gianluc95@hotmail.it>
2021-01-24 18:14:56 +0100

    Remove category on message, change message format

--- src/commands/hint.js
+++ src/commands/hint.js
@@ -2,66 +2,40 @@ const Commands = require('../core/command');
 
 module.exports = class Hint extends Commands {
 	constructor(client) {
-		super(client);
+        super(client);
 		this.cmd = 'hint';
-		this.alias = 'hint';
-		this.args =
-      '[tipologia (Opzionale)] Messaggio di suggerimento.';
-		this.example = `${client.conf.prefix}hint [nuovo canale] Creazione di un canale per l'argomento X`;
-		this.description =
-      'Suggerimenti per migliorare il server di CodersHub';
-		this.timer = 0;
-		this.access = [client._botSettings.rules.everyone];
-		this.displayHelp = 1;
-		this.client = client;
+        this.alias = 'hint';
+        this.args = '';
+        this.example = `${client.conf.prefix}proposta creare un canale per discutere di nodejs e delle sue funzinoalità`;
+        this.description = 'Suggerimenti per migliorare il server di CodersHub';
+        this.timer = 0;
+        this.access = [client._botSettings.rules.everyone];
+        this.displayHelp = 1;
+        this.client = client;
 	}
 
 	async execution(message, bot) {
 		const id_author = message.author.id;
-		const channelName = 'suggerimenti';
-		let hintMessage = message.args;
-		let hintCategory = '';
+		const hintChannel = this.client.channels.cache.find(channel => channel.name === 'suggerimenti');
+        let hintMessage = message.args;
+        
+        if (hintMessage.length < 5) {
+			message.reply(' il messaggio inserito deve avere più di 5 caratteri');
+        }
+        else {
+            const emb = new bot._botMessageEmbed();
+            emb.setTitle(`Nuovo suggerimento da ${(message.author.username)? message.author.username : message.author.name }`);
+            emb.setDescription(hintMessage);
+            emb.setColor('RANDOM');
+            emb.setTimestamp();
+            const messageToSend =await hintChannel.send(emb).catch((e) => {
+                console.log(e)
+            });
+            await messageToSend.react('👍');
+            await messageToSend.react('👎');
 
-		if (hintMessage.indexOf('[', 0) == 0 && hintMessage.indexOf(']', 0) > 0) {
-			console.log(hintMessage.indexOf('[', 0), '[');
-			console.log(hintMessage.indexOf(']', 0), ']');
-			hintCategory = hintMessage.substring(hintMessage.indexOf('[', 0) + 1, hintMessage.indexOf(']', 0));
-			const hintCategoryLast = hintMessage.indexOf(']', 0);
-			// Elimino la parte della categoria
-			hintMessage = hintMessage.substr(hintCategoryLast + 1);
-		}
-
-		// Questo per togliere lo spazio dopo [tipologia]
-		if (hintMessage.indexOf(' ', 0) == 0) {
-			hintMessage = hintMessage.substr(1);
-		}
-
-		const hintChannel = this.client.channels.cache.find(channel => channel.name === channelName);
-
-		let resp_str = '';
-
-		if (hintCategory) {
-			resp_str = `Suggerimento per la categoria **${hintCategory}** condiviso da <@${id_author}>:`;
-		}
-		else {
-			resp_str = `Suggerimento inviato da <@${id_author}>:`;
-		}
-
-		if (hintMessage.length < 1) {
-			message.reply(' il messaggio inserito deve avere più di 1 carattere');
-		}
-		else {
-			const messageToSend = await hintChannel
-				.send(resp_str + '\n' + hintMessage)
-				.catch((e) => {
-					console.log(e);
-				});
-
-			await messageToSend.react('👍');
-			await messageToSend.react('👎');
-
-			await message.delete();
-		}
-
+            message.reply(' la proposta è stato inviata. Grazie per aver contribuito a migliorare la Community!');
+            await message.delete();
+        }
 	}
 };
