0538f90988e579783fad3336db5c675328704fe1
David <davidgalet@gmail.com>
2021-01-28 10:16:06 +0100

    Nascondo i test. (Temporaneo)

--- tests/handlers/index.js
+++ tests/handlers/index.js
@@ -1,5 +1,24 @@
+const { rest } = require('msw')
 const channelsHandlers = require('./discord/channels')
 const rolesHandlers = require('./discord/roles')
 const guildsHandlers = require('./discord/guilds')
 
-module.exports = [...channelsHandlers, ...rolesHandlers, ...guildsHandlers]
+module.exports = [
+  ...channelsHandlers,
+  ...rolesHandlers,
+  ...guildsHandlers,
+  rest.get('*/api/:apiVersion/gateway/bot', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        url: '',
+        shards: 1,
+        session_start_limit: {
+          total: 1000,
+          remaining: 999,
+          reset_after: 14400000,
+        },
+      }),
+    )
+  }),
+]
