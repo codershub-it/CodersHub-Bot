271a040618b4971b963336c689d6fc42301062f2
David <davidgalet@gmail.com>
2021-01-30 15:46:52 +0100

    Aggiornato il sistema embed fix errore

--- src/commands/embed/embed.js
+++ src/commands/embed/embed.js
@@ -5,8 +5,7 @@ module.exports = class Embed extends Commands {
     super(client)
     this.cmd = 'embed'
     this.alias = 'em'
-    this.args =
-      "[#nome_canale (Opzionale)] Questo parametro serve a far stampare il messaggio in un'altro canale"
+    this.args = '[Oggetto messaggio] [#nome_canale (Opzionale)]'
     this.example = `${client.conf.prefix}embed 123456789123`
     this.description =
       'Con questo comando scrivi un messaggio in embed allegando il file .json. Usa questo sito https://leovoel.github.io/embed-visualizer per creare il file json.'
@@ -16,54 +15,62 @@ module.exports = class Embed extends Commands {
   }
 
   async execution(message, bot) {
-    // Prendo id del canale
-    const channelId = message.args.replace(/[^0-9]/g, '')
-
+    let channel_id = null
+    // Estraggo il canale menzionato
+    if (message.mentions) {
+      message.mentions._channels.map((testChannel) => {
+        channel_id = testChannel.id
+      })
+    }
     // Verifico la presenza del canale
-    if (!bot.channels.cache.get(channelId) && channelId) {
+    if (!bot.channels.cache.get(channel_id) && channel_id) {
       message.reply(' il canale non esiste in questo server')
       return
     }
 
+    let jsonFile = {}
     // Estrapolo la lista file allegati
     const messageAttachments = message.attachments.toJSON()
-    let embed = null
-
     // Estraggo i file allegati, il primo.
     if (messageAttachments.isArray || messageAttachments.length > 0) {
       const _maFileUrl = messageAttachments[0].url
       const _nameFile = messageAttachments[0].name
-
       // Verifico il formato del file
       if (_nameFile.split('.').pop() !== 'json') {
         // Errore formato file
         message.reply(' il formato del file è errato')
+        return
       }
-
       // Estraggo i dati dal file remoto
       if (_maFileUrl) {
         try {
-          embed = await (await bot._botFetch(_maFileUrl)).json()
+          jsonFile = await (await bot._botFetch(_maFileUrl)).json()
         } catch (e) {
           // Errore caricamento file
           message.reply(
             ` non è stato possibile caricare il file usa **${bot.conf.prefix}embed_exemple** per scaricare la demo`,
           )
+          return
         }
       }
     } else {
       message.reply(
         ` non hai caricato nessun file .json, **${bot.conf.prefix}embed_exemple** per scaricare la demo`,
       )
+      return
     }
+
+    const embed = jsonFile.embed
+
     // Invia il messaggio al canale specifico
-    if (channelId) {
-      bot.channels.cache.get(channelId).send({ embed })
-      message.reply(` messaggio inviato con successo nel canale: <#${channelId}>`)
-      embed = null
+    if (channel_id) {
+      bot.channels.cache.get(channel_id).send(jsonFile.content, { embed })
+      message.reply(` messaggio inviato con successo nel canale: <#${channel_id}>`)
+      return
     }
-    if (embed) {
-      message.channel.send({ embed }).catch((e) => {
+
+    if (jsonFile) {
+      message.channel.send(jsonFile.content, { embed }).catch((e) => {
         console.log(e)
       })
     }
