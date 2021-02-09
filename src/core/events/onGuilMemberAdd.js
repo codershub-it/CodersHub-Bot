582ae6f55baf0eec8ec746e76cc56b60ae9af94a
Federico Gambarino <federico.gambarino@deltatre.it>
2021-02-09 11:10:33 +0100

    removed wrong channel id

--- src/core/events/onGuilMemberAdd.js
+++ src/core/events/onGuilMemberAdd.js
@@ -2,7 +2,7 @@ function init(client) {
   client.on('guildMemberAdd', async (member) => {
     try {
       await addSettingsRoles(client, member)
-      await sendPmToNewMember(member)
+      await sendPmToNewMember(client._botSettings.channel.impostazione_ruoli_id, member)
     } catch (e) {
       console.log('Errore al guildMemberAdd', e)
     }
@@ -26,12 +26,11 @@ async function addSettingsRoles(client, member) {
   }
 }
 
-async function sendPmToNewMember(member) {
+async function sendPmToNewMember(settingsChannelId, member) {
   await member.user.send('Ciao!', {
     embed: {
       title: 'Benvenuto su CodersHub!',
-      description:
-        "Se vuoi scegliere quali sezioni vedere e che tipo di notifiche ricevere all'interno del nostro server, puoi andare nel canale <#805724080111353916> e personalizzare le impostazioni",
+      description: `Se vuoi scegliere quali sezioni vedere e che tipo di notifiche ricevere all'interno del nostro server, puoi andare nel canale <#${settingsChannelId}> e personalizzare le impostazioni`,
       color: 'RANDOM',
       thumbnail: {
         url: 'https://media.tenor.com/images/7ab5c8247e639abe8a5bb6de0f2bcf76/tenor.gif',
